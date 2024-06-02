3e211dc2bdc651f4103c96f71c164b79c59a94b6
toliadriapak <toliadriapak@gmail.com>
2024-06-02 17:30:50 +0300

    Aded coments and icon

--- DocTemplateGen/Docs.Designer.cs
+++ DocTemplateGen/Docs.Designer.cs
@@ -2,24 +2,25 @@
 {
     partial class MainForm
     {
-        private System.ComponentModel.IContainer components = null;
-        private System.Windows.Forms.ListBox listBoxTemplates;
-        private System.Windows.Forms.TextBox txtOutputPath;
-        private System.Windows.Forms.TextBox txtFirstName;
-        private System.Windows.Forms.TextBox txtLastName;
-        private System.Windows.Forms.TextBox txtEmail;
-        private System.Windows.Forms.TextBox txtProductName;
-        private System.Windows.Forms.TextBox txtDiscount;
-        private System.Windows.Forms.TextBox txtExpiryDate;
-        private System.Windows.Forms.Button btnGenerate;
-        private System.Windows.Forms.Button btnBrowse;
-        private System.Windows.Forms.Label lblFirstName;
-        private System.Windows.Forms.Label lblLastName;
-        private System.Windows.Forms.Label lblEmail;
-        private System.Windows.Forms.Label lblProductName;
-        private System.Windows.Forms.Label lblDiscount;
-        private System.Windows.Forms.Label lblExpiryDate;
+        private System.ComponentModel.IContainer components = null; // Component container
+        private System.Windows.Forms.ListBox listBoxTemplates; // ListBox for template selection
+        private System.Windows.Forms.TextBox txtOutputPath; // TextBox for output path
+        private System.Windows.Forms.TextBox txtFirstName; // TextBox for first name
+        private System.Windows.Forms.TextBox txtLastName; // TextBox for last name
+        private System.Windows.Forms.TextBox txtEmail; // TextBox for email
+        private System.Windows.Forms.TextBox txtProductName; // TextBox for product name
+        private System.Windows.Forms.TextBox txtDiscount; // TextBox for discount
+        private System.Windows.Forms.TextBox txtExpiryDate; // TextBox for expiry date
+        private System.Windows.Forms.Button btnGenerate; // Button to generate document
+        private System.Windows.Forms.Button btnBrowse; // Button to browse output path
+        private System.Windows.Forms.Label lblFirstName; // Label for first name
+        private System.Windows.Forms.Label lblLastName; // Label for last name
+        private System.Windows.Forms.Label lblEmail; // Label for email
+        private System.Windows.Forms.Label lblProductName; // Label for product name
+        private System.Windows.Forms.Label lblDiscount; // Label for discount
+        private System.Windows.Forms.Label lblExpiryDate; // Label for expiry date
 
+        // Dispose method to clean up resources
         protected override void Dispose(bool disposing)
         {
             if (disposing && (components != null))
@@ -29,6 +30,7 @@
             base.Dispose(disposing);
         }
 
+        // Initialize components and form layout
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
@@ -68,53 +70,53 @@
             // 
             // txtFirstName
             // 
-            this.txtFirstName.Location = new System.Drawing.Point(12, 171);
+            this.txtFirstName.Location = new System.Drawing.Point(12, 170);
             this.txtFirstName.Name = "txtFirstName";
             this.txtFirstName.Size = new System.Drawing.Size(200, 22);
             this.txtFirstName.TabIndex = 2;
             // 
             // txtLastName
             // 
-            this.txtLastName.Location = new System.Drawing.Point(12, 215);
+            this.txtLastName.Location = new System.Drawing.Point(12, 210);
             this.txtLastName.Name = "txtLastName";
             this.txtLastName.Size = new System.Drawing.Size(200, 22);
             this.txtLastName.TabIndex = 3;
             // 
             // txtEmail
             // 
-            this.txtEmail.Location = new System.Drawing.Point(12, 259);
+            this.txtEmail.Location = new System.Drawing.Point(12, 250);
             this.txtEmail.Name = "txtEmail";
             this.txtEmail.Size = new System.Drawing.Size(200, 22);
             this.txtEmail.TabIndex = 4;
             // 
             // txtProductName
             // 
-            this.txtProductName.Location = new System.Drawing.Point(12, 303);
+            this.txtProductName.Location = new System.Drawing.Point(12, 290);
             this.txtProductName.Name = "txtProductName";
             this.txtProductName.Size = new System.Drawing.Size(200, 22);
             this.txtProductName.TabIndex = 5;
             // 
             // txtDiscount
             // 
-            this.txtDiscount.Location = new System.Drawing.Point(12, 347);
+            this.txtDiscount.Location = new System.Drawing.Point(12, 330);
             this.txtDiscount.Name = "txtDiscount";
             this.txtDiscount.Size = new System.Drawing.Size(200, 22);
             this.txtDiscount.TabIndex = 6;
             // 
             // txtExpiryDate
             // 
-            this.txtExpiryDate.Location = new System.Drawing.Point(12, 391);
+            this.txtExpiryDate.Location = new System.Drawing.Point(12, 370);
             this.txtExpiryDate.Name = "txtExpiryDate";
             this.txtExpiryDate.Size = new System.Drawing.Size(200, 22);
             this.txtExpiryDate.TabIndex = 7;
             // 
             // btnGenerate
             // 
-            this.btnGenerate.Location = new System.Drawing.Point(222, 458);
+            this.btnGenerate.Location = new System.Drawing.Point(12, 470);
             this.btnGenerate.Name = "btnGenerate";
-            this.btnGenerate.Size = new System.Drawing.Size(386, 29);
-            this.btnGenerate.TabIndex = 8;
-            this.btnGenerate.Text = "Згенерувати";
+            this.btnGenerate.Size = new System.Drawing.Size(776, 37);
+            this.btnGenerate.TabIndex = 14;
+            this.btnGenerate.Text = "Generate Document";
             this.btnGenerate.UseVisualStyleBackColor = true;
             this.btnGenerate.Click += new System.EventHandler(this.btnGenerate_Click);
             // 
@@ -123,70 +125,70 @@
             this.btnBrowse.Location = new System.Drawing.Point(676, 429);
             this.btnBrowse.Name = "btnBrowse";
             this.btnBrowse.Size = new System.Drawing.Size(112, 23);
-            this.btnBrowse.TabIndex = 9;
-            this.btnBrowse.Text = "Огляд...";
+            this.btnBrowse.TabIndex = 15;
+            this.btnBrowse.Text = "Browse...";
             this.btnBrowse.UseVisualStyleBackColor = true;
             this.btnBrowse.Click += new System.EventHandler(this.btnBrowse_Click);
             // 
             // lblFirstName
             // 
             this.lblFirstName.AutoSize = true;
-            this.lblFirstName.Location = new System.Drawing.Point(12, 152);
+            this.lblFirstName.Location = new System.Drawing.Point(12, 150);
             this.lblFirstName.Name = "lblFirstName";
-            this.lblFirstName.Size = new System.Drawing.Size(32, 16);
-            this.lblFirstName.TabIndex = 10;
-            this.lblFirstName.Text = "Ім\'я:";
+            this.lblFirstName.Size = new System.Drawing.Size(75, 16);
+            this.lblFirstName.TabIndex = 8;
+            this.lblFirstName.Text = "First Name:";
             // 
             // lblLastName
             // 
             this.lblLastName.AutoSize = true;
-            this.lblLastName.Location = new System.Drawing.Point(12, 196);
+            this.lblLastName.Location = new System.Drawing.Point(12, 190);
             this.lblLastName.Name = "lblLastName";
-            this.lblLastName.Size = new System.Drawing.Size(72, 16);
-            this.lblLastName.TabIndex = 11;
-            this.lblLastName.Text = "Прізвище:";
+            this.lblLastName.Size = new System.Drawing.Size(75, 16);
+            this.lblLastName.TabIndex = 9;
+            this.lblLastName.Text = "Last Name:";
             // 
             // lblEmail
             // 
             this.lblEmail.AutoSize = true;
-            this.lblEmail.Location = new System.Drawing.Point(12, 240);
+            this.lblEmail.Location = new System.Drawing.Point(12, 230);
             this.lblEmail.Name = "lblEmail";
             this.lblEmail.Size = new System.Drawing.Size(44, 16);
-            this.lblEmail.TabIndex = 12;
+            this.lblEmail.TabIndex = 10;
             this.lblEmail.Text = "Email:";
             // 
             // lblProductName
             // 
             this.lblProductName.AutoSize = true;
-            this.lblProductName.Location = new System.Drawing.Point(12, 284);
+            this.lblProductName.Location = new System.Drawing.Point(12, 270);
             this.lblProductName.Name = "lblProductName";
-            this.lblProductName.Size = new System.Drawing.Size(117, 16);
-            this.lblProductName.TabIndex = 13;
-            this.lblProductName.Text = "Назва продукту:";
+            this.lblProductName.Size = new System.Drawing.Size(96, 16);
+            this.lblProductName.TabIndex = 11;
+            this.lblProductName.Text = "Product Name:";
             // 
             // lblDiscount
             // 
             this.lblDiscount.AutoSize = true;
-            this.lblDiscount.Location = new System.Drawing.Point(12, 328);
+            this.lblDiscount.Location = new System.Drawing.Point(12, 310);
             this.lblDiscount.Name = "lblDiscount";
-            this.lblDiscount.Size = new System.Drawing.Size(59, 16);
-            this.lblDiscount.TabIndex = 14;
-            this.lblDiscount.Text = "Знижка:";
+            this.lblDiscount.Size = new System.Drawing.Size(62, 16);
+            this.lblDiscount.TabIndex = 12;
+            this.lblDiscount.Text = "Discount:";
             // 
             // lblExpiryDate
             // 
             this.lblExpiryDate.AutoSize = true;
-            this.lblExpiryDate.Location = new System.Drawing.Point(11, 372);
+            this.lblExpiryDate.Location = new System.Drawing.Point(12, 350);
             this.lblExpiryDate.Name = "lblExpiryDate";
-            this.lblExpiryDate.Size = new System.Drawing.Size(118, 16);
-            this.lblExpiryDate.TabIndex = 15;
-            this.lblExpiryDate.Text = "Дата закінчення:";
+            this.lblExpiryDate.Size = new System.Drawing.Size(79, 16);
+            this.lblExpiryDate.TabIndex = 13;
+            this.lblExpiryDate.Text = "Expiry Date:";
             // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 500);
+            this.ClientSize = new System.Drawing.Size(800, 520);
             this.Controls.Add(this.lblExpiryDate);
             this.Controls.Add(this.lblDiscount);
             this.Controls.Add(this.lblProductName);
@@ -205,7 +207,7 @@
             this.Controls.Add(this.listBoxTemplates);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "MainForm";
-            this.Text = "Генератор документів";
+            this.Text = "Document Generator";
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.MainForm_FormClosing);
             this.ResumeLayout(false);
             this.PerformLayout();
---
+++ DocTemplateGen/Docs.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Windows.Forms;
+using Word = Microsoft.Office.Interop.Word;
+
+namespace DocTemplateGen
+{
+    public partial class MainForm : Form
+    {
+        private Word.Application wordApp; // Word application instance
+        private List<string> templates; // List to store template file paths
+
+        public MainForm()
+        {
+            InitializeComponent();
+            wordApp = new Word.Application(); // Initialize Word application
+            templates = new List<string>();
+
+            LoadTemplates(); // Load templates on form initialization
+        }
+
+        // Method to load template files from the specified directory
+        private void LoadTemplates()
+        {
+            string templatePath = @"D:\my project vs\DocTemplateGen\DocTemplateGen\Templates"; // Path to templates
+            if (Directory.Exists(templatePath))
+            {
+                templates.AddRange(Directory.GetFiles(templatePath, "*.dotx")); // Add .dotx files to templates list
+                listBoxTemplates.DataSource = templates; // Bind templates list to ListBox
+            }
+            else
+            {
+                MessageBox.Show("Templates not found!"); // Show error if directory doesn't exist
+            }
+        }
+
+        // Event handler for Generate button click
+        private void btnGenerate_Click(object sender, EventArgs e)
+        {
+            if (listBoxTemplates.SelectedItem == null)
+            {
+                MessageBox.Show("Please select a template!"); // Show error if no template is selected
+                return;
+            }
+
+            string templateFile = listBoxTemplates.SelectedItem.ToString(); // Get selected template file path
+            string outputPath = txtOutputPath.Text; // Get output path from text box
+
+            if (string.IsNullOrEmpty(outputPath))
+            {
+                MessageBox.Show("Please specify an output path!"); // Show error if output path is empty
+                return;
+            }
+
+            GenerateDocument(templateFile, outputPath); // Call method to generate document
+        }
+
+        // Method to generate document from the selected template
+        private void GenerateDocument(string templateFile, string outputPath)
+        {
+            try
+            {
+                Word.Document doc = wordApp.Documents.Add(templateFile); // Add new document from template
+                // Find and replace placeholders with user input
+                FindAndReplace(doc, "<FIRST_NAME>", txtFirstName.Text);
+                FindAndReplace(doc, "<LAST_NAME>", txtLastName.Text);
+                FindAndReplace(doc, "<EMAIL>", txtEmail.Text);
+                FindAndReplace(doc, "<PRODUCT_NAME>", txtProductName.Text);
+                FindAndReplace(doc, "<DISCOUNT>", txtDiscount.Text);
+                FindAndReplace(doc, "<EXPIRY_DATE>", txtExpiryDate.Text);
+
+                doc.SaveAs2(outputPath); // Save the document to the specified output path
+                doc.Close(); // Close the document
+                MessageBox.Show("Document saved successfully!");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message); // Show error message if an exception occurs
+            }
+        }
+
+        // Method to find and replace text in the document
+        private void FindAndReplace(Word.Document doc, object findText, object replaceWithText)
+        {
+            foreach (Word.Range range in doc.StoryRanges) // Iterate through all story ranges in the document
+            {
+                Word.Find find = range.Find;
+                find.Text = findText.ToString(); // Set text to find
+                find.Replacement.Text = replaceWithText.ToString(); // Set replacement text
+                find.Execute(Replace: Word.WdReplace.wdReplaceAll); // Execute find and replace
+            }
+        }
+
+        // Event handler for Browse button click
+        private void btnBrowse_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog
+            {
+                Filter = "Word Documents|*.docx", // Set file filter for SaveFileDialog
+                Title = "Save Document"
+            };
+            if (saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                txtOutputPath.Text = saveFileDialog.FileName; // Set output path text box to selected file path
+            }
+        }
+
+        // Event handler for form closing
+        private void MainForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            wordApp.Quit(); // Quit Word application when form is closing
+        }
+    }
+}
